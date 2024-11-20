07b31508e84b2b7462ebca86bed25c4044a62462
malick <bmalick.gning@gmail.com>
2024-11-20 16:06:10 +0100

    review code; add test; logging; feature task deletion as i am soooooo lazyyy

---
+++ tests/create_database.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('.')
+from src.notion import NotionApiHandler
+
+if __name__=="__main__":
+    handler = NotionApiHandler()
+    properties = {
+        "Description": "text", "Checkbox": "checkbox",
+        "Date": "date", "Email": "email",
+        "File": "files", "Formula": "formula",
+        "Number": "number", "People": "people",
+        "Phone Number": "phone_number", "URL": "url",
+    }
+    # properties = {"name": n, "type": t for n,t in properties.items()}
+    # properties = {**properties,
+    #               }
+        # "Select": "select",
+        # "Multi Select": "multi_select",
+    handler.create_database(
+        parent_id=handler.keys["api_handler_page_id"], 
+        db_title="Database creation test",
+        icon="https://www.notion.so/icons/database_gray.svg",
+        properties=properties
+    )
